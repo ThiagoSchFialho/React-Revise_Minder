257124874e59cbec575d6361b002d3a75dc1bf0c
Thiago Scheffer Fialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2025-02-28 14:19:08 -0300

    feat: Fast form button

--- src/pages/StudyForm/StudyForm.tsx
+++ src/pages/StudyForm/StudyForm.tsx
@@ -4,8 +4,10 @@ import Header from '../../components/Header/Header';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import { useDb } from '../../hooks/useDb';
+import { PiLightningFill } from "react-icons/pi";
 import {
     MainContainer,
+    FastFormbutton,
     FormContainer,
     FormTitle,
     InputContainer,
@@ -48,8 +50,9 @@ const formatDate = (date: string) => {
 
 const StudyForm: React.FC = () => {
     const { createStudy, getStudy, updateStudy } = useDb();
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const { id } = useParams<{ id: string }>();
+    const [isFastFormSelected, setIsFastFormSelected] = useState<boolean>(true);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isToday, setIsToday] = useState<boolean>(false);
     const [initialValues, setInitialValues] = useState<FormValues>({
         topic: '',
@@ -71,6 +74,13 @@ const StudyForm: React.FC = () => {
                 })
             }
             fetchStudies();
+        } else {
+            setInitialValues({
+                topic: '',
+                qnt_reviews: 0,
+                study_date: '',
+                user_id: Number(localStorage.getItem('userId'))
+            })
         }
     }, [id])
 
@@ -94,70 +104,86 @@ const StudyForm: React.FC = () => {
             <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
             <MainContainer isMenuOpen={isMenuOpen}>
                 <FormTitle>{id ? 'Editar' : 'Adicionar'} estudo</FormTitle>
-                <FormContainer>
-                    <Formik
-                        initialValues={initialValues}
-                        enableReinitialize={true}
-                        validationSchema={validations}
-                        onSubmit={(values) => handleSubmit(values)}
+                {!id && (
+                    <FastFormbutton
+                        selected={isFastFormSelected}
+                        onClick={() => setIsFastFormSelected(!isFastFormSelected)}
                     >
-                    {({values, handleChange, touched, errors, setFieldValue }) => (
-                        <Form>
-                            <InputContainer>
-                                <Label htmlFor='topic'>Tópico</Label>
-                                <Input 
-                                    type="text"
-                                    name="topic"
-                                    id="topic"
-                                    onChange={handleChange}
-                                    value={values.topic}
-                                />
-                                {touched.topic && errors.topic && <Error>{errors.topic}</Error>}
-                            </InputContainer>
-    
-                            <InputContainer>
-                                <Label htmlFor='qnt_reviews'>Quantidade de revisões</Label>
-                                <Input 
-                                    type="number"
-                                    min='0'
-                                    name="qnt_reviews"
-                                    id="qnt_reviews"
-                                    onChange={handleChange}
-                                    value={values.qnt_reviews}
-                                />
-                                {touched.qnt_reviews && errors.qnt_reviews && <Error>{errors.qnt_reviews}</Error>}
-                            </InputContainer>
+                        <PiLightningFill size={24} />
+                        Formulário rápido
+                    </FastFormbutton>
+                )}
 
-                            <InputContainer>
-                                <Label htmlFor='study_date'>Data do estudo</Label>
-                                <DateInputContainer>
-                                    <TodayButton
-                                        selected={isToday}
-                                        onClick={() => {
-                                            setIsToday(!isToday);
-                                            handleTodayClick(setFieldValue);
-                                        }}
-                                    >
-                                        Hoje
-                                    </TodayButton>
-                                    <Input
-                                        type="date"
-                                        name="study_date"
-                                        id="study_date"
+                {isFastFormSelected || id ? (
+                    <FormContainer>
+                        <Formik
+                            initialValues={initialValues}
+                            enableReinitialize={true}
+                            validationSchema={validations}
+                            onSubmit={(values) => handleSubmit(values)}
+                        >
+                        {({values, handleChange, touched, errors, setFieldValue }) => (
+                            <Form>
+                                <InputContainer>
+                                    <Label htmlFor='topic'>Tópico</Label>
+                                    <Input 
+                                        type="text"
+                                        name="topic"
+                                        id="topic"
                                         onChange={handleChange}
-                                        value={values.study_date}
-                                        disabled={isToday ? true : false}
+                                        value={values.topic}
                                     />
-                                </DateInputContainer>
-                                {touched.study_date && errors.study_date && <Error>{errors.study_date}</Error>}
-                            </InputContainer>
+                                    {touched.topic && errors.topic && <Error>{errors.topic}</Error>}
+                                </InputContainer>
+        
+                                <InputContainer>
+                                    <Label htmlFor='qnt_reviews'>Quantidade de revisões</Label>
+                                    <Input 
+                                        type="number"
+                                        min='0'
+                                        name="qnt_reviews"
+                                        id="qnt_reviews"
+                                        onChange={handleChange}
+                                        value={values.qnt_reviews}
+                                    />
+                                    {touched.qnt_reviews && errors.qnt_reviews && <Error>{errors.qnt_reviews}</Error>}
+                                </InputContainer>
 
-                            <SubmitButton type='submit'>{id ? 'Salvar' : 'Adicionar'}</SubmitButton>
-                        </Form>
-                    )}
+                                <InputContainer>
+                                    <Label htmlFor='study_date'>Data do estudo</Label>
+                                    <DateInputContainer>
+                                        <TodayButton
+                                            selected={isToday}
+                                            onClick={() => {
+                                                setIsToday(!isToday);
+                                                handleTodayClick(setFieldValue);
+                                            }}
+                                        >
+                                            Hoje
+                                        </TodayButton>
+                                        <Input
+                                            type="date"
+                                            name="study_date"
+                                            id="study_date"
+                                            onChange={handleChange}
+                                            value={values.study_date}
+                                            disabled={isToday ? true : false}
+                                        />
+                                    </DateInputContainer>
+                                    {touched.study_date && errors.study_date && <Error>{errors.study_date}</Error>}
+                                </InputContainer>
+
+                                <SubmitButton type='submit'>{id ? 'Salvar' : 'Adicionar'}</SubmitButton>
+                            </Form>
+                        )}
+                            
+                        </Formik>
+                    </FormContainer>
+                ) : (
+                    <FormContainer>
                         
-                    </Formik>
-                </FormContainer>
+                    </FormContainer>
+                )}
             </MainContainer>
         </>
     )
--- src/pages/StudyForm/styles.ts
+++ src/pages/StudyForm/styles.ts
@@ -11,6 +11,30 @@ export const MainContainer = styled.div<{ isMenuOpen?: boolean }>`
     }
 `;
 
+export const FastFormbutton = styled.div<{ selected: boolean }>`
+    position: absolute;
+    right: 90px;
+    top: 105px;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    border: 1px solid ${({theme}) => theme.mainColor};;
+    background-color: ${(props) => props.selected ? ({theme}) => theme.mainColor : ''};
+    color: ${(props) => props.selected ? 'white' : ({theme}) => theme.text};
+    font-size: 15pt;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-weight: 500;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+
+    @media (max-width: 1360px) {
+        font-size: 13pt;
+    }
+`;
+
 export const FormContainer = styled.div`
     width: 430px;
     margin: 0 auto;
@@ -28,8 +52,11 @@ export const FormContainer = styled.div`
 export const FormTitle = styled.h1`
     color: ${({ theme }) => theme.text};
     text-align: center;
-    margin-bottom: 25px;
+    margin-bottom: 80px;
 
+    @media (max-width: 1360px) {
+        margin-bottom: 25px;
+    }
 `;
 
 export const InputContainer = styled.div``;
