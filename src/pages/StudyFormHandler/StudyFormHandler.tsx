1ed4b2e904cfea2c5630be8b2ff5f292584e937c
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2025-03-03 20:52:58 -0300

    Update StudyFormHandler.tsx

--- src/pages/StudyFormHandler/StudyFormHandler.tsx
+++ src/pages/StudyFormHandler/StudyFormHandler.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../../components/Header/Header'
 import { PiLightningFill } from "react-icons/pi";
 import StudyForm from '../../components/Forms/StudyForm/StudyForm';
 import BegginerForm from '../../components/Forms/BegginerStudyForm/BegginerStudyForm';
+import { useDb } from '../../hooks/useDb';
 import {
     MainContainer,
     FastFormbutton,
@@ -12,15 +13,26 @@ import {
 
 const StudyFormHandler: React.FC = () => {
     const { id } = useParams<{ id?: string }>();
+    const { getUser } = useDb();
     const [isFastFormSelected, setIsFastFormSelected] = useState<boolean>(false);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [qntStudiesAdded, setQntStudiesAdded] = useState<number>(0);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const user = await getUser();
+            console.log(user);
+            setQntStudiesAdded(user.qnt_studies_added);
+        }
+        fetchUser();
+    }, [])
 
     return (
         <>
             <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
             <MainContainer>
                 <FormTitle>{id ? 'Editar' : 'Adicionar'} estudo</FormTitle>
-                {!id && (
+                {!id && qntStudiesAdded > 0 && (
                     <FastFormbutton
                         selected={isFastFormSelected}
                         onClick={() => setIsFastFormSelected(!isFastFormSelected)}
