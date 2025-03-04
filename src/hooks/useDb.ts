45ae5921b45bd775679b1edd5104e8ac1a2032f2
Thiago Scheffer Fialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2025-03-04 19:29:14 -0300

    Update useDb.ts

--- src/hooks/useDb.ts
+++ src/hooks/useDb.ts
@@ -6,7 +6,7 @@ interface StudyValues {
 }
 
 export const useDb = () => {
-    const host = 'https://revise-minder-backend.onrender.com';
+    const host = 'https://localhost:3000';
 
     const addQntStudyAdded = async () => {
         const token = localStorage.getItem('token');
