f24ff55fcb88d5a5469fba2c7dca443f5d85ee1b
Thiago Scheffer Fialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2025-03-04 19:53:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  preview: {
-    allowedHosts: ['react-revise-minder.onrender.com'],
-  },
-  server: {
-    host: '0.0.0.0',
-  },
+  plugins: [react()]
 })
